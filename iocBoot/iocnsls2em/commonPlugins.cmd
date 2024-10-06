485858481184c923ea1a1079c685f9c831bd3183
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2024-10-06 11:21:57 -0400

    Enable netCDF1 plugin

--- iocBoot/iocnsls2em/commonPlugins.cmd
+++ iocBoot/iocnsls2em/commonPlugins.cmd
@@ -79,9 +79,8 @@ NDTimeSeriesConfigure("$(PORT)_STATS11_TS", $(QSIZE), 0, "$(PORT)_STATS11", 1, 2
 dbLoadRecords("$(ADCORE)/db/NDTimeSeries.template",  "P=$(SYS),R={$(DEV)}$(T11):TS:, PORT=$(PORT)_STATS11_TS,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT)_STATS11,NDARRAY_ADDR=1,NCHANS=$(TSPOINTS),ENABLED=1")
 
 # Create a netCDF file saving plugin.
-#NDFileNetCDFConfigure("$(PORT)_FileNetCDF1", $(QSIZE), 0, "$(PORT)", 11)
-#dbLoadRecords("$(ADCORE)/db/NDFileNetCDF.template","P=$(SYS),R=${(DEV)}netCDF1:,PORT=$(PORT)_FileNetCDF1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=11,ENABLED=0")
-
+NDFileNetCDFConfigure("$(PORT)_FileNetCDF1", $(QSIZE), 0, "$(PORT)", 11)
+dbLoadRecords("$(ADCORE)/db/NDFileNetCDF.template","P=$(SYS),R={$(DEV)}netCDF1:,PORT=$(PORT)_FileNetCDF1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=11,ENABLED=0")
 
 # This creates a waveform large enough for 11x10000 arrays.
 NDStdArraysConfigure("$(PORT)_Image1", $(QSIZE), 0, "$(PORT)", 11)
