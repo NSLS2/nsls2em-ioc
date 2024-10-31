511e9b8eeadef61c179d4eab2de79b0b23d928df
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2024-10-31 16:27:20 -0400

    First implementation of the script that generates calibrations

---
+++ calibration/E0009.cmd
@@ -0,0 +1,63 @@
+#########################################################
+# Calibration for NSLS2 Electrometer: serial number E0009
+#########################################################
+
+# Calibration for 10mA range
+dbpf ${PREFIX}ChA-10mA-Gain-SP 516930
+dbpf ${PREFIX}ChB-10mA-Gain-SP 514985
+dbpf ${PREFIX}ChC-10mA-Gain-SP 514646
+dbpf ${PREFIX}ChD-10mA-Gain-SP 518055
+dbpf ${PREFIX}ChA-10mA-Ofst-SP 940
+dbpf ${PREFIX}ChB-10mA-Ofst-SP 2796
+dbpf ${PREFIX}ChC-10mA-Ofst-SP 2355
+dbpf ${PREFIX}ChD-10mA-Ofst-SP 1832
+
+# Calibration for 1ma range
+dbpf ${PREFIX}ChA-1ma-Gain-SP 516175
+dbpf ${PREFIX}ChB-1ma-Gain-SP 513379
+dbpf ${PREFIX}ChC-1ma-Gain-SP 513778
+dbpf ${PREFIX}ChD-1ma-Gain-SP 514914
+dbpf ${PREFIX}ChA-1ma-Ofst-SP 938
+dbpf ${PREFIX}ChB-1ma-Ofst-SP 2795
+dbpf ${PREFIX}ChC-1ma-Ofst-SP 2351
+dbpf ${PREFIX}ChD-1ma-Ofst-SP 1830
+
+# Calibration for 100uA range
+dbpf ${PREFIX}ChA-100uA-Gain-SP 515412
+dbpf ${PREFIX}ChB-100uA-Gain-SP 513776
+dbpf ${PREFIX}ChC-100uA-Gain-SP 515277
+dbpf ${PREFIX}ChD-100uA-Gain-SP 516310
+dbpf ${PREFIX}ChA-100uA-Ofst-SP 949
+dbpf ${PREFIX}ChB-100uA-Ofst-SP 2805
+dbpf ${PREFIX}ChC-100uA-Ofst-SP 2355
+dbpf ${PREFIX}ChD-100uA-Ofst-SP 1837
+
+# Calibration for 10uA range
+dbpf ${PREFIX}ChA-10uA-Gain-SP 516488
+dbpf ${PREFIX}ChB-10uA-Gain-SP 513262
+dbpf ${PREFIX}ChC-10uA-Gain-SP 514955
+dbpf ${PREFIX}ChD-10uA-Gain-SP 515767
+dbpf ${PREFIX}ChA-10uA-Ofst-SP 983
+dbpf ${PREFIX}ChB-10uA-Ofst-SP 2834
+dbpf ${PREFIX}ChC-10uA-Ofst-SP 2391
+dbpf ${PREFIX}ChD-10uA-Ofst-SP 1874
+
+# Calibration for 1uA range
+dbpf ${PREFIX}ChA-1uA-Gain-SP 518169
+dbpf ${PREFIX}ChB-1uA-Gain-SP 516026
+dbpf ${PREFIX}ChC-1uA-Gain-SP 515075
+dbpf ${PREFIX}ChD-1uA-Gain-SP 517705
+dbpf ${PREFIX}ChA-1uA-Ofst-SP 982
+dbpf ${PREFIX}ChB-1uA-Ofst-SP 2829
+dbpf ${PREFIX}ChC-1uA-Ofst-SP 2386
+dbpf ${PREFIX}ChD-1uA-Ofst-SP 1865
+
+# Calibration for 100nA range
+dbpf ${PREFIX}ChA-100nA-Gain-SP 520229
+dbpf ${PREFIX}ChB-100nA-Gain-SP 516189
+dbpf ${PREFIX}ChC-100nA-Gain-SP 514206
+dbpf ${PREFIX}ChD-100nA-Gain-SP 518567
+dbpf ${PREFIX}ChA-100nA-Ofst-SP 993
+dbpf ${PREFIX}ChB-100nA-Ofst-SP 2837
+dbpf ${PREFIX}ChC-100nA-Ofst-SP 2395
+dbpf ${PREFIX}ChD-100nA-Ofst-SP 1877
